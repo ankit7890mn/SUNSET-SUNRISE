2b850d22a0cd8b229efe60620fd36467d5faba75
ankit7890mn <78220892+ankit7890mn@users.noreply.github.com>
2021-06-26 08:42:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,17 +30,7 @@ function draw(){
 
     // write code to display time in correct format here
     
-    fill("black");
-    textSize(30);
-
-    if(hour>=12){
-        text("Time : "+ hour%12 + " PM", 50,100);
-       }else if(hour==0){
-         text("Time : 12 AM",100,100);
-       }else{
-        text("Time : "+ hour%12 + " AM", 50,100);
-       }
-
+    
 
 }
 
